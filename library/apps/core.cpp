3ad4e25366c83117db228abfb10b5798ce0e390b
Finn Glas <finn@finnmglas.com>
2026-01-26 19:40:00 +0100

    fix: code alignment

--- library/apps/core.cpp
+++ library/apps/core.cpp
@@ -1,25 +1,24 @@
 #include "core.h"
 
 // import all apps / pages here at once
+// outcommented stuff is in main ino file
 // #include "library/apps/all.h"
-#include "library/apps/boot.cpp"    // page: boot screen & setup
-//#include "library/apps/core.cpp"    // page: core utils for page / menu setup
-
-#include "library/apps/measurement.cpp"  // page: measurement helpers
-
-#include "library/apps/about.cpp"   // page: about (message)
-#include "library/apps/random.cpp"  // page: random colors screen test
-#include "library/apps/wifiscan.cpp"  // page: wifi scan
-#include "library/apps/btscan.cpp"  // page: bluethooth scan
-#include "library/apps/colortest.cpp"  // page: gradient color test page
-#include "library/apps/colorwheeltest.cpp"  // page: color test page
-#include "library/apps/snakegame.cpp"  // page: snake game
-#include "library/apps/gol.cpp"  // page: conways game of life
-#include "library/apps/mandelbrot.cpp"  // page: mandelbrot set render
-#include "library/apps/wireframe3d.cpp"  // page: 3d rotation renderer
-#include "library/apps/aicuflow_tutorial.cpp"  // page: tutorial
+// #include "library/apps/core.cpp"           // page: core utils for page / menu setup
+#include "library/apps/boot.cpp"              // page: boot screen & setup
+#include "library/apps/measurement.cpp"       // page: measurement helpers
+#include "library/apps/about.cpp"             // page: about (message)
+#include "library/apps/random.cpp"            // page: random colors screen test
+#include "library/apps/wifiscan.cpp"          // page: wifi scan
+#include "library/apps/btscan.cpp"            // page: bluethooth scan
+#include "library/apps/colortest.cpp"         // page: gradient color test page
+#include "library/apps/colorwheeltest.cpp"    // page: color test page
+#include "library/apps/snakegame.cpp"         // page: snake game
+#include "library/apps/gol.cpp"               // page: conways game of life
+#include "library/apps/mandelbrot.cpp"        // page: mandelbrot set render
+#include "library/apps/wireframe3d.cpp"       // page: 3d rotation renderer
+#include "library/apps/aicuflow_tutorial.cpp" // page: tutorial
 // add more pages here
-// #include "library/apps/_expand.cpp" // page: custom page
+// #include "library/apps/_expand.cpp"        // page: custom page
 
 
 // UI / App Core of this Sketch
